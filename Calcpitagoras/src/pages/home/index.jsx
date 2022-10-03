1985b26347631aee7faf525b56bf2496632fb92a
Mariella Pugens <mariellapugens@gmail.com>
2022-10-03 00:03:57 -0300

    implementa Calcular novamente

--- Calcpitagoras/src/pages/home/index.jsx
+++ Calcpitagoras/src/pages/home/index.jsx
@@ -4,6 +4,8 @@ import './styles.css';
 
 function Home() {
 
+  const [result, setResult] = useState(0)
+
   const [values, setValues] = useState({
     catetoAdjacente: '',
     catetoOposto: '',
@@ -48,22 +50,22 @@ function Home() {
 function calc(catetoOposto, catetoAdjacente, hipotenusa) {
   if (hipotenusa === '1') {
     var hipotenusa = Math.hypot(catetoAdjacente,catetoOposto)
-    console.log(hipotenusa)  
+    setResult(hipotenusa)  
   } else if (catetoAdjacente === '1') {
     var ladoaaoquadrado = Math.pow(hipotenusa,2) - Math.pow(catetoOposto,2)
     var ladoa = Math.pow(ladoaaoquadrado,0.5) 
-    console.log(ladoa);    
+    setResult(ladoa)
   } else if (catetoOposto === '1') {
     var ladobaoquadrado = Math.pow(hipotenusa,2) - Math.pow(catetoAdjacente,2)
     var ladob = Math.pow(ladobaoquadrado,0.5) 
-    console.log(ladob);
+    setResult(ladob)
   }else{
       setValues({
         catetoOposto: '',
         catetoAdjacente: '',
         hipotenusa: ''
       });
-      alert("Não foi possível efetuar o calculo, insira os valores novamente");
+      alert("Não foi possível efetuar o cálculo, insira os valores novamente");
   }
 }
 
@@ -71,13 +73,19 @@ function calc(catetoOposto, catetoAdjacente, hipotenusa) {
   return (
     <div className="container">
       <h1>Calculadora Pitágoras</h1>
-      <form onSubmit={onSubmitHandler} className="form" id="form">
+      {result === 0 && <form onSubmit={onSubmitHandler} className="form" id="form">
         <input onChange={handleCatetoOpostoInputChange} className="co" id="co" placeholder="Valor do cateto oposto" min='1' type="number" value={values.catetoOposto}/>
         <input onChange={handleCatetoAdjacenteInputChange} className="ca" id="ca" placeholder="Valor do cateto adjacente" min='1' type="number" value={values.catetoAdjacente} />
         <input onChange={handleHipotenusaInputChange} className="hipotenusa" id="hp" placeholder="Valor da hipotenusa"   min='1' type="number" value={values.hipotenusa}/>
-      <button type='submit'>Calcular</button>
-    </form>
-    
+        <button type='submit'>Calcular</button>
+     </form>}
+      {result !== 0 &&
+      <div>
+        <p>{result}</p>
+          <button onClick={() => setResult(0)}>Calcular novamente</button>
+      </div>
+      }
+
     </div>
   )
 }
