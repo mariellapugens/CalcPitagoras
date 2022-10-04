f592662ef6a0f245cda2dd695c94301f553ea0cc
Mariella Pugens <mariellapugens@gmail.com>
2022-10-03 23:49:39 -0300

    implementa controle para somente um x

--- Calcpitagoras/src/pages/home/index.jsx
+++ Calcpitagoras/src/pages/home/index.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import './styles.css';
+import { useState } from 'react'
+import './styles.css'
 
 
 function Home() {
@@ -8,56 +8,107 @@ function Home() {
 
   const [result, setResult] = useState(0)
 
-  const [values, setValues] = useState({
-    catetoAdjacente: '',
-    catetoOposto: '',
-    hipotenusa: '',
-  });
- 
-  const handleCatetoAdjacenteInputChange = (event) => {
-    event.persist();
-    setValues((prevState) => ({
-      ...prevState,
-      catetoAdjacente: event.target.value,
-    }));
-  };
+  const initialValuesState = {
+    catetoAdjacente: {value: '', isX: false},
+    catetoOposto: {value: '', isX: false},
+    hipotenusa: {value: '', isX: false},
+  }
 
-  const handleCatetoOpostoInputChange = (event) => {
-    event.persist();
-    setValues((prevState) => ({
-      ...prevState,
-      catetoOposto: event.target.value,
-    }));
-  };
-
-  const handleHipotenusaInputChange = (event) => {
-    event.persist();
-    setValues((prevState) => ({
-      ...prevState,
-      hipotenusa: event.target.value,
-    }));
-  };
+  const [values, setValues] = useState(initialValuesState)
 
   const inputCheck = (input) => {
     if (typeof input === 'string' && input === 'x' || input === 'X') {
-      return input.toLowerCase()
-    }else if (Number(input)) {
-      return input
-    }else {
-      alert("Insira um valor númerico ou x")
-    }
+     
+       return input.toLowerCase()
+     }else if (Number(input)) {
+       return input
+     }else {
+ 
+       setValues(initialValuesState)
+       alert("Insira um valor númerico ou x")
+     }
+   }
+ 
+  const handleCatetoAdjacenteInputChange = (event) => {
+    const newValue = inputCheck(event.target.value)
+
+    if( (values.catetoOposto.isX || values.hipotenusa.isX) && newValue === 'x' ){
+    alert('Você só poder ter um X da questão')
+   } else {
+    setValues((prevState) => {      
+      const newIsX = newValue === 'x' ? !prevState.catetoAdjacente.isX : prevState.catetoAdjacente.isX      
+    return({
+    ...prevState,
+    catetoAdjacente: {...prevState.catetoAdjacente, value: newValue, isX: newIsX},
+  })})
+   }
   }
+
+  const handleCatetoOpostoInputChange = (event) => {
+    const newValue = inputCheck(event.target.value)
+
+    if( (values.catetoAdjacente.isX || values.hipotenusa.isX) && newValue === 'x' ){
+      alert('Você só poder ter um X da questão')
+    } else {
+      setValues((prevState) => {      
+        const newIsX = newValue === 'x' ? !prevState.catetoOposto.isX : prevState.catetoOposto.isX      
+      return({
+      ...prevState,
+      catetoOposto: {...prevState.catetoOposto, value: newValue, isX: newIsX},
+    })})
+    }
+
+    
+  }
+
+  const handleHipotenusaInputChange = (event) => {
+    const newValue = inputCheck(event.target.value)
+
+    if( (values.catetoOposto.isX || values.catetoAdjacente.isX) && newValue === 'x' ){
+      alert('Você só poder ter um X da questão')
+    } else {
+      setValues((prevState) => {      
+        const newIsX = newValue === 'x' ? !prevState.hipotenusa.isX : prevState.hipotenusa.isX      
+      return({
+      ...prevState,
+      hipotenusa: {...prevState.hipotenusa, value: newValue, isX: newIsX},
+    })})
+    }
+
+    
+  }
+
+  
+
+
+  
+
+  function limpaInputs() {
+      setValues(initialValuesState)
+
+      alert("Não foi possível efetuar o cálculo, insira os valores novamente")
+  }
+
+
 
   const onSubmitHandler = (event) => {
     event.preventDefault()
 
-    const  catetoOposto = inputCheck(event.target.co.value) 
-    console.log(catetoOposto)   
-    const catetoAdjacente = inputCheck(event.target.ca.value)
-    const hipotenusa = inputCheck(event.target.hp.value)   
- 
-    calc(catetoOposto, catetoAdjacente, hipotenusa)
+    console.log('values.catetoOposto.isX', values.catetoOposto.isX)
+    console.log('values.catetoAdjacente.isX', values.catetoAdjacente.isX)
+    console.log('values.hipotenusa.isX', values.hipotenusa.isX)
 
+
+    
+
+    const catetoOposto = inputCheck(values.catetoOposto.value) 
+    const catetoAdjacente = inputCheck(values.catetoAdjacente.value)
+    const hipotenusa = inputCheck(values.hipotenusa.value)
+
+     
+      calc(catetoOposto, catetoAdjacente, hipotenusa)
+
+      setValues(initialValuesState)    
   }
   
 function calc(catetoOposto, catetoAdjacente, hipotenusa) {
@@ -65,30 +116,24 @@ function calc(catetoOposto, catetoAdjacente, hipotenusa) {
 
 
   if (hipotenusa === 'x') {
-    var hipotenusaTratada = "0";
+    var hipotenusaTratada = "0"
     var hipotenusaTratada = Math.hypot(catetoAdjacente,catetoOposto)
     setResultadoCalculo("O valor da hipotenusa é")
     setResult(hipotenusaTratada)
   } else if (catetoAdjacente === 'x') {
-    var catetoAdjacenteTratado = "0";
+    var catetoAdjacenteTratado = "0"
     var catetoAdjacenteTratado = Math.pow(hipotenusa,2) - Math.pow(catetoOposto,2)
     var ladoA = Math.pow(catetoAdjacenteTratado,0.5)
     setResultadoCalculo("O valor do cateto adjacente é") 
     setResult(ladoA)
   } else if ('x' === catetoOposto) {
     var catetoOpostoTratado = "0"
-    console.log(catetoOpostoTratado);
     var catetoOpostoTratado = Math.pow(hipotenusa,2) - Math.pow(catetoAdjacente,2)
     var ladoB = Math.pow(catetoOpostoTratado,0.5)
     setResultadoCalculo('O valor do cateto oposto é')
     setResult(ladoB)
   }else{
-      setValues({
-        catetoOposto: '',
-        catetoAdjacente: '',
-        hipotenusa: ''
-      });
-      alert("Não foi possível efetuar o cálculo, insira os valores novamente");
+    limpaInputs()
   }
 }
 
@@ -97,15 +142,15 @@ function calc(catetoOposto, catetoAdjacente, hipotenusa) {
     <div className="container">
       <h1>Calculadora Pitágoras</h1>
       {result === 0 && <form onSubmit={onSubmitHandler} className="form" id="form">
-        <input onChange={handleCatetoOpostoInputChange} className="co" id="co" placeholder="Valor do cateto oposto" min='1' type="text" value={values.catetoOposto}/>
-        <input onChange={handleCatetoAdjacenteInputChange} className="ca" id="ca" placeholder="Valor do cateto adjacente" min='1' type="text" value={values.catetoAdjacente} />
-        <input onChange={handleHipotenusaInputChange} className="hipotenusa" id="hp" placeholder="Valor da hipotenusa"   min='1' type="text" value={values.hipotenusa}/>
+        <input onChange={handleCatetoOpostoInputChange} className="co" id="co" placeholder="Valor do cateto oposto" min='1'  value={values.catetoOposto.value}/>
+        <input onChange={handleCatetoAdjacenteInputChange} className="ca" id="ca" placeholder="Valor do cateto adjacente" min='1' value={values.catetoAdjacente.value} />
+        <input onChange={handleHipotenusaInputChange} className="hipotenusa" id="hp" placeholder="Valor da hipotenusa"   min='1'  value={values.hipotenusa.value}/>
         <button type='submit'>Calcular</button>
      </form>}
       {result !== 0 &&
       <div className='resultado'>
         <p>{resultadoCalculo} {result}</p>
-          <button onClick={() => setResult(0)}s>Calcular novamente</button>
+          <button onClick={() => (setResult(0), setValues(initialValuesState))}>Calcular novamente</button>
       </div>
       }
 
